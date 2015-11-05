97f2ab687d28173e6cf1f889387eb4ee88ff273e
arunjyothi <arunjyothi86@gmail.com>
2015-11-05 09:49:25 +0530

    iOS database implementation
    iOS database implementation

---
+++ purposecollor-local/PurposeColor/PurposeColor/PurposeColor.iOS/Dependency/DBConnection.cs
@@ -0,0 +1,25 @@
+﻿using PurposeColor.interfaces;
+
+[assembly: Xamarin.Forms.Dependency(typeof(PurposeColor.iOS.Dependency.DBConnection))]
+namespace PurposeColor.iOS.Dependency
+{
+    public class DBConnection : IDBConnection
+    {
+        public DBConnection()
+        {
+        }
+
+        public SQLite.Net.SQLiteConnection GetConnection()
+        {
+            var fileName = "PurposeColorDB.db3";
+            var documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var libraryPath = System.IO.Path.Combine(documentsPath, "..", "Library");
+            var path = System.IO.Path.Combine(libraryPath, fileName);
+            var platform = new SQLite.Net.Platform.XamarinIOS.SQLitePlatformIOS();
+            var connection = new SQLite.Net.SQLiteConnection(platform, path);
+
+            return connection;
+
+        }
+    }
+}

29f1f60134895e794c0ddf6d40708799a96c63f9
arunjyothi <purposecode@Purposes-Mac-mini.local>
2015-11-13 19:25:16 +0530

    iOS audio recording
    iOS audio recording

--- purposecollor-local/PurposeColor/PurposeColor/PurposeColor.iOS/Dependency/AudioRecorder.cs
+++ purposecollor-local/PurposeColor/PurposeColor/PurposeColor.iOS/Dependency/AudioRecorder.cs
@@ -23,30 +23,34 @@ namespace PurposeColor.iOS.Dependency
 				var audioSession = AVFoundation.AVAudioSession.SharedInstance ();
 				var err = audioSession.SetCategory (AVFoundation.AVAudioSessionCategory.PlayAndRecord);
 				if (err != null) {
-					Console.WriteLine ("audioSession: {0}", err);
 					return false;
 				}
 				err = audioSession.SetActive (true);
 				if (err != null) {
-					Console.WriteLine ("audioSession: {0}", err);
 					return false;
 				}
-				// ------done initialising ---
-				
-				string fileName = string.Format ("Myfile{0}.wav", DateTime.Now.ToString ("yyyyMMddHHmmss"));
-				string audioFilePath = System.IO.Path.Combine (System.IO.Path.GetTempPath (), fileName);
-				
-				Console.WriteLine ("Audio File Path: " + audioFilePath);
-				
+				string directoryname = string.Empty;
+
+				try {
+					var documents =
+						Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments);
+					directoryname = System.IO.Path.Combine (documents, "PurposeColor/Audio");
+					System.IO.Directory.CreateDirectory(directoryname);
+				} catch (Exception ex) {
+					var test = ex.Message;
+				}
+
+				string fileName = string.Format ("Audio{0}.wav", DateTime.Now.ToString ("yyyyMMddHHmmss"));
+				string audioFilePath = System.IO.Path.Combine (directoryname, fileName);
+
 				url = Foundation.NSUrl.FromFilename (audioFilePath);
+
 				//set up the NSObject Array of values that will be combined with the keys to make the NSDictionary
 				Foundation.NSObject[] values = new NSObject[] {
 					NSNumber.FromFloat (44100.0f), //Sample Rate
 					NSNumber.FromInt32 ((int)AudioToolbox.AudioFormatType.LinearPCM), //AVFormat
-					NSNumber.FromInt32 (2), //Channels
-					NSNumber.FromInt32 (16), //PCMBitDepth
-					NSNumber.FromBoolean (false), //IsBigEndianKey
-					NSNumber.FromBoolean (false) //IsFloatKey
+					NSNumber.FromInt32 (1), //Channels
+					NSNumber.FromInt32 (16) //PCMBitDepth
 				};
 				
 				//Set up the NSObject Array of keys that will be combined with the values to make the NSDictionary
@@ -54,24 +58,19 @@ namespace PurposeColor.iOS.Dependency
 					AVAudioSettings.AVSampleRateKey,
 					AVAudioSettings.AVFormatIDKey,
 					AVAudioSettings.AVNumberOfChannelsKey,
-					AVAudioSettings.AVLinearPCMBitDepthKey,
-					AVAudioSettings.AVLinearPCMIsBigEndianKey,
-					AVAudioSettings.AVLinearPCMIsFloatKey
+					AVAudioSettings.AVLinearPCMBitDepthKey
 				};
-				
 				//Set Settings with the Values and Keys to create the NSDictionary
 				settings = NSDictionary.FromObjectsAndKeys (values, keys);
-				
+				var error  = new NSError();
 				//Set recorder parameters
 				recorder = AVAudioRecorder.Create (url, new AudioSettings (settings), out error);
-				
 				//Set Recorder to Prepare To Record
 				recorder.PrepareToRecord ();
-				
 				recorder.Record ();
 
 				return true;
-			} 
+			}
 			catch (Exception ex) 
 			{
 				var tt = ex.Message;
@@ -88,14 +87,11 @@ namespace PurposeColor.iOS.Dependency
 		public void PlayAudio()
 		{
 			try {
-				// var file = Path.Combine("sounds", filename);
-				//var Soundurl = NSUrl.FromFilename(file);
-
-				//var Soundurl = NSUrl.FromFilename (url);
 				var error  = new NSError();
-				player = new AVAudioPlayer(url,null,out error);
+				if (player != null) {
+					
+				}
 				player = AVAudioPlayer.FromUrl (url);
-
 				player.NumberOfLoops = 1;
 				player.Volume = 1.0f;
 				player.FinishedPlaying += DidFinishPlaying;
@@ -112,10 +108,9 @@ namespace PurposeColor.iOS.Dependency
 		{
 			if (e.Status)
 			{
-				// your code
+				GC.Collect ();
 			}
 		}
-
 	}
 }
 

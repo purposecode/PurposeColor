ab98af35b5b32b5c9f203384d8eb29a281bdda28
arunjyothi <arunjyothi86@gmail.com>
2015-11-16 12:30:56 +0530

    images added
    images added

--- purposecollor-local/PurposeColor/PurposeColor/PurposeColor/screens/AudioRecorderPage.cs
+++ purposecollor-local/PurposeColor/PurposeColor/PurposeColor/screens/AudioRecorderPage.cs
@@ -2,80 +2,90 @@
 using Xamarin.Forms;
 using CustomControls;
 using PurposeColor.interfaces;
+using PurposeColor.CustomControls;
 
 namespace PurposeColor
 {
-	public class AudioRecorderPage : ContentPage
-	{
-		CustomLayout masterLayout;
-		IAudioRecorder audioRecorder;
-		public AudioRecorderPage ()
-		{
-			NavigationPage.SetHasNavigationBar(this, false);
-
-			audioRecorder = DependencyService.Get<IAudioRecorder>();
-			masterLayout = new CustomLayout();
-			masterLayout.BackgroundColor = Color.Gray;
+    public class AudioRecorderPage : ContentPage
+    {
+        CustomLayout masterLayout;
+        IAudioRecorder audioRecorder;
+        public AudioRecorderPage()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            CustomLayout masterLayout = new CustomLayout();
+            masterLayout.HorizontalOptions = LayoutOptions.Center;
+            masterLayout.BackgroundColor = Color.FromRgb(230, 255, 254);
+            PurposeColorSubTitleBar subTitleBar = new PurposeColorSubTitleBar(Color.FromRgb(12, 113, 210), "Audio recorder", false);
+            masterLayout.AddChildToLayout(subTitleBar, 0, Device.OnPlatform(2, 4, 4));
 
             PurposeColor.CustomControls.PurposeColorTitleBar titleBar = new PurposeColor.CustomControls.PurposeColorTitleBar(Color.FromRgb(8, 137, 216), "Purpose Color", Color.Black, "back");
-			masterLayout.AddChildToLayout(titleBar, 0, 0);
-
-			Button recordBtn = new Button 
-			{
-				Text = "Record"
-			};
-			recordBtn.Clicked += RecordBtn_Clicked;
-			masterLayout.AddChildToLayout(recordBtn, 0, 40);
-
-			Button stopRecordBtn = new Button 
-			{
-				Text = "Stop Recording"
-			};
-			stopRecordBtn.Clicked += StopRecordBtn_Clicked;
-			masterLayout.AddChildToLayout(stopRecordBtn, 0, 50);
-
-
-			Button PlaybackBtn = new Button 
-			{
-				Text = "Playback"
-			};
-			PlaybackBtn.Clicked +=	PlaybackBtn_Clicked;
-			masterLayout.AddChildToLayout(PlaybackBtn, 0, 60);
             masterLayout.AddChildToLayout(titleBar, 0, 0);
 
+            Button recordBtn = new Button
+            {
+                Text = "Record",
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            recordBtn.Clicked += RecordBtn_Clicked;
+            masterLayout.AddChildToLayout(recordBtn, 10, 20);
 
-			Content = masterLayout;
-		}
+            Button stopRecordBtn = new Button
+            {
+                Text = "Stop Recording",
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            stopRecordBtn.Clicked += StopRecordBtn_Clicked;
+            masterLayout.AddChildToLayout(stopRecordBtn, 10, 30);
 
-		void StopRecordBtn_Clicked (object sender, EventArgs e)
-		{
-			audioRecorder.StopRecording ();
-		}
 
-		void RecordBtn_Clicked (object sender, EventArgs e)
-		{
+            Button PlaybackBtn = new Button
+            {
+                Text = "Playback",
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.Center
+            };
+            PlaybackBtn.Clicked += PlaybackBtn_Clicked;
+            masterLayout.AddChildToLayout(PlaybackBtn, 10, 40);
+
+
+
+
+            Content = masterLayout;
+        }
+
+        void StopRecordBtn_Clicked(object sender, EventArgs e)
+        {
+            audioRecorder.StopRecording();
+        }
+
+        void RecordBtn_Clicked(object sender, EventArgs e)
+        {
             if (audioRecorder == null)
             {
                 audioRecorder = DependencyService.Get<IAudioRecorder>();
             }
-			bool isAudioRecording = audioRecorder.RecordAudio ();
-			if (isAudioRecording == false) {
-				DisplayAlert ("Audio recording", "Sorry the audio recording service is not available now, please try again later", "OK");
-			}
-			else
-			{
-				DisplayAlert ("Audio recording", "Audio recording stared", "OK");
-			}
-		}
+            bool isAudioRecording = audioRecorder.RecordAudio();
+            if (isAudioRecording == false)
+            {
+                DisplayAlert("Audio recording", "Sorry the audio recording service is not available now, please try again later", "OK");
+            }
+            else
+            {
+                DisplayAlert("Audio recording", "Audio recording stared", "OK");
+            }
+        }
 
-		void backButton_Clicked(object sender, System.EventArgs e)
-		{
-			//Navigation.PushAsync( new GraphPage() );
-		}
-		void PlaybackBtn_Clicked(object sender, System.EventArgs e)
-		{
-			audioRecorder.PlayAudio ();
-		}
-	}
+        void backButton_Clicked(object sender, System.EventArgs e)
+        {
+            //Navigation.PushAsync( new GraphPage() );
+        }
+        void PlaybackBtn_Clicked(object sender, System.EventArgs e)
+        {
+            audioRecorder.PlayAudio();
+        }
+    }
 }
 
---
+++ purposecollor-local/PurposeColor/PurposeColor/PurposeColor/screens/EventsSituationsThoughts/AddEventsSituationsOrThoughts.cs
@@ -0,0 +1,33 @@
+﻿using CustomControls;
+using PurposeColor.CustomControls;
+using Xamarin.Forms;
+
+namespace PurposeColor.screens
+{
+    public class AddEventsSituationsOrThoughts : ContentPage
+    {
+        public AddEventsSituationsOrThoughts()
+        {
+            NavigationPage.SetHasNavigationBar(this, false);
+            CustomLayout masterLayout = new CustomLayout();
+            masterLayout.BackgroundColor = Color.FromRgb(230, 255, 254);
+
+            PurposeColorSubTitleBar subTitleBar = new PurposeColorSubTitleBar(Color.FromRgb(12, 113, 210), "Emotional Awareness", false);
+            masterLayout.AddChildToLayout(subTitleBar, 0, Device.OnPlatform(2, 4, 4));
+
+            Editor textInput = new Editor
+            {
+                VerticalOptions = LayoutOptions.StartAndExpand,
+                HorizontalOptions = LayoutOptions.StartAndExpand
+            };
+
+            masterLayout.AddChildToLayout(textInput, 10, 20);
+            Entry entry = new Entry
+            {
+                Placeholder = "placeholder"
+            };
+
+            Content = masterLayout;
+        }
+    }
+}

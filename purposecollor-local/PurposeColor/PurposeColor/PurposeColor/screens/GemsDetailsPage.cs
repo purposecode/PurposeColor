1c628dc1374eee3b1e00283a698bfb156c8c1397
praveenmohanmm <praveen.mm.online@gmail.com>
2016-01-11 17:14:22 +0530

    windows video player added
    windows video player added

--- purposecollor-local/PurposeColor/PurposeColor/PurposeColor/screens/GemsDetailsPage.cs
+++ purposecollor-local/PurposeColor/PurposeColor/PurposeColor/screens/GemsDetailsPage.cs
@@ -397,11 +397,8 @@ namespace PurposeColor
                 string fileName = Path.GetFileName(img.ClassId);
                 if (fileName != null)
                 {
-                    if (Device.OS != TargetPlatform.WinPhone)
-                    {
-                        IVideoDownloader videoDownload = DependencyService.Get<IVideoDownloader>();
-                        videoDownload.Download(img.ClassId, fileName);
-                    }
+                    IVideoDownloader videoDownload = DependencyService.Get<IVideoDownloader>();
+                    videoDownload.Download(img.ClassId, fileName);
 
                 }
 
@@ -416,11 +413,8 @@ namespace PurposeColor
                 string fileName = Path.GetFileName(img.ClassId);
                 if (fileName != null)
                 {
-                    if (Device.OS != TargetPlatform.WinPhone)
-                    {
-                        IVideoDownloader videoDownload = DependencyService.Get<IVideoDownloader>();
-                        videoDownload.Download(img.ClassId, fileName);
-                    }
+                    IVideoDownloader videoDownload = DependencyService.Get<IVideoDownloader>();
+                    videoDownload.Download(img.ClassId, fileName);
                 }
 
             }

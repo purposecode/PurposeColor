2ab5c5d279f6e40727250a9bd0e1a78a8ce42acb
arunjyothi <purposecode@Purposes-Mac-mini.local>
2016-01-23 18:15:00 +0530

    iOS img rotate
    iOS img rotate

--- purposecollor-local/PurposeColor/PurposeColor/PurposeColor.iOS/Dependency/IOSResize.cs
+++ purposecollor-local/PurposeColor/PurposeColor/PurposeColor.iOS/Dependency/IOSResize.cs
@@ -37,10 +37,96 @@ namespace PurposeColor.iOS.Dependency
 
                 UIKit.UIImage resizedImage = UIKit.UIImage.FromImage(context.ToImage());
 
+				//UIImage rotated = this.RotateImage( resizedImage , true);
+				resizedImage = UIImage.FromImage(resizedImage.CGImage, resizedImage.CurrentScale, UIImageOrientation.Right);
                 // save the image as a jpeg
-                return resizedImage.AsJPEG().ToArray();
+				return resizedImage.AsJPEG().ToArray();
             }
         }
+
+
+		public UIImage RotateImage( UIImage imageToRotate, bool isCCW)
+		{
+			UIImage rotatedImage = new UIImage ();
+
+			var imageRotation = isCCW ? UIImageOrientation.Right : UIImageOrientation.Left;
+			rotatedImage = UIImage.FromImage(imageToRotate.CGImage, imageToRotate.CurrentScale, UIImageOrientation.Right);
+			return rotatedImage;
+		}  
+
+
+		public static UIImage ScaleAndRotateImage( UIImage image)
+		{
+			int kMaxResolution = 1280; // Or whatever
+
+			CGImage imgRef = image.CGImage;
+			float width = imgRef.Width;
+			float height = imgRef.Height;
+			CGAffineTransform transform = CGAffineTransform.MakeIdentity();
+			RectangleF bounds = new RectangleF(0, 0, width, height);
+
+			if (width > kMaxResolution || height > kMaxResolution)
+			{
+				float ratio = width / height;
+
+				if (ratio > 1)
+				{
+					bounds.Size = new SizeF(kMaxResolution, bounds.Size.Width / ratio);
+				}
+				else
+				{
+					bounds.Size = new SizeF(bounds.Size.Height * ratio, kMaxResolution);
+				}
+			}
+
+			float scaleRatio = bounds.Size.Width / width;
+			SizeF imageSize = new SizeF(imgRef.Width, imgRef.Height);
+			UIImageOrientation orient = image.Orientation;
+			float boundHeight;
+
+			switch (orient)
+			{
+			case UIImageOrientation.Up:                                        //EXIF = 1
+				boundHeight = bounds.Size.Height;
+				bounds.Size = new SizeF (boundHeight, bounds.Size.Width);
+				transform = CGAffineTransform.MakeRotation (180); 
+				//transform = CGAffineTransform.Rotate(transform, (float)Math.PI / 90.0f);
+				break;
+				// TODO: Add other Orientations
+			case UIImageOrientation.Right:                                     //EXIF = 8
+				boundHeight = bounds.Size.Height;
+				bounds.Size = new SizeF(boundHeight, bounds.Size.Width);
+				transform = CGAffineTransform.MakeTranslation(imageSize.Height, 0);
+				transform = CGAffineTransform.Rotate(transform, (float)Math.PI / 2.0f);
+				break;
+			default:
+				throw new Exception("Invalid image orientation");                        
+
+			}
+
+			UIGraphics.BeginImageContext(bounds.Size);
+
+			CGContext context = UIGraphics.GetCurrentContext();
+
+			if (orient == UIImageOrientation.Right || orient == UIImageOrientation.Left)
+			{
+				context.ScaleCTM(-scaleRatio, scaleRatio);
+				context.TranslateCTM(-height, 0);
+			}
+			else
+			{
+				context.ScaleCTM(scaleRatio, -scaleRatio);
+				context.TranslateCTM(0, -height);
+			}
+
+			context.ConcatCTM(transform);
+
+			context.DrawImage(new RectangleF(0, 0, width, height), imgRef);
+			UIImage imageCopy = UIGraphics.GetImageFromCurrentImageContext();
+			UIGraphics.EndImageContext();
+
+			return imageCopy;
+		}
 
 
 		public MemoryStream CompessImage(int ratio, MemoryStream ms)

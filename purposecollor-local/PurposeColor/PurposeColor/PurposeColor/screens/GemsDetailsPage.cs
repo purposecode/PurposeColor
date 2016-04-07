55bb73d53ef6d6240591b71ec5eb8a2c662c0aca
praveenmohanmm <praveen.mm.online@gmail.com>
2016-04-07 16:23:34 +0530

    s6 fix
    s6 fix

--- purposecollor-local/PurposeColor/PurposeColor/PurposeColor/screens/GemsDetailsPage.cs
+++ purposecollor-local/PurposeColor/PurposeColor/PurposeColor/screens/GemsDetailsPage.cs
@@ -259,7 +259,7 @@ namespace PurposeColor
 								aspect = imgSize.Height / imgSize.Width;
 								img.HeightRequest = App.screenWidth * aspect;
 								if( aspect > 1.5 )
-									img.HeightRequest = App.screenWidth * 1.25;
+									img.HeightRequest = App.screenWidth * 1.50;
 							}
 							else
 							{
@@ -378,7 +378,7 @@ namespace PurposeColor
 								aspect = imgSize.Height / imgSize.Width;
 								img.HeightRequest = App.screenWidth * aspect;
 								if( aspect > 1.5 )
-									img.HeightRequest = App.screenWidth * 1.25;
+									img.HeightRequest = App.screenWidth * 1.50;
 							}
 							else
 							{
@@ -499,7 +499,7 @@ namespace PurposeColor
 								aspect = imgSize.Height / imgSize.Width;
 								img.HeightRequest = App.screenWidth * aspect;
 								if( aspect > 1.5 )
-									img.HeightRequest = App.screenWidth * 1.25;
+									img.HeightRequest = App.screenWidth * 1.50;
 							}
 							else
 							{
